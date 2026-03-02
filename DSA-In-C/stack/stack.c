8807d9db1c718d486e4bdf442178c6a95ac5b110
Yuyi-Hao <roshanisingh2354@gmail.com>
2026-03-02 21:08:11 +0530

    [DSA] Fix stack implementation

--- DSA-In-C/stack/stack.c
+++ DSA-In-C/stack/stack.c
@@ -6,6 +6,6 @@
 int stack_push(Stack *stack, const void *data){
     return list_insert_next(stack, NULL, data);
 }
-int stack_pop(Stack *stack, const void *data){
+int stack_pop(Stack *stack, void **data){
     return list_remove_next(stack, NULL, data);
 }
