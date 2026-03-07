bd22d7af7c5727db79d8a5a53c5e379cc690615a
Yuyi-Hao <roshanisingh2354@gmail.com>
2026-03-07 21:27:30 +0530

    [FIX] Macro error in pop

--- Leetcode/queue/00001_simple_queue.h
+++ Leetcode/queue/00001_simple_queue.h
@@ -6,6 +6,6 @@ int q[CAP];
 int front = 0, rear = 0;
 
 #define push(q, x, front, rear)  q[rear] = (x), rear = (rear + 1) % CAP
-#define pop(q, front, rear)      (q[front]), front = (front + 1) % CAP
+#define pop(q, front, rear)      (q[front++ % CAP])
 #define empty(front, rear)       (front == rear)
 #define size(front, rear)        ((rear - front + CAP) % CAP)
